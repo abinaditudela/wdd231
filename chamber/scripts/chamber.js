71b53493187f0a569ada17115d7503fa3392a12a
abinaditudela <abinadi.tc@gmail.com>
2024-12-08 03:54:32 -0400

    W03 Assignment: Chamber Home Page - Finished

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -4,14 +4,15 @@ document.getElementById('menu-toggle').addEventListener('click', () => {
 });
 
 // Weather API Integration
-const apiKey = "YOUR_API_KEY";
+const apiKey = "857456a7252a4af33fdf44440c4ec1d5";//"YOUR_API_KEY";
 const city = "Cochabamba";
 const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 async function fetchWeather() {
     const response = await fetch(apiUrl);
     const data = await response.json();
-    document.getElementById("current-temp").textContent = data.main.temp.toFixed(0);
+    console.log(data.main.temp);
+    document.getElementById("current-temp").textContent = data.main.temp.toFixed(0) + ` °C`;
     document.getElementById("weather-icon").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
     document.getElementById("weather-description").textContent = capitalizeWords(data.weather[0].description);
 }
@@ -32,16 +33,21 @@ fetch("data/members.json")
 
 function displaySpotlights(members) {
     const spotlightContainer = document.getElementById("spotlight-cards");
+    
+    // Filtrar miembros con membresía "Gold" o "Premium"
     const filteredMembers = members.filter(
-        (member) => member.membership === "gold" || member.membership === "silver"
+        (member) => member.membership === "Gold" || member.membership === "Premium"
     );
+    
+    // Asegurarnos de mostrar solo 3 miembros
     filteredMembers.slice(0, 3).forEach((member) => {
         spotlightContainer.innerHTML += `
             <div class="business-card">
-                <img src="${member.image}" alt="${member.names}">
-                <h3>${member.names}</h3>
-                <p>${member.addresses}</p>
+                <img src="${member.logo}" alt="${member.name}">
+                <h3>${member.name}</h3>
+                <p>${member.address}</p>
                 <p>${member.phone}</p>
             </div>`;
     });
 }
+
